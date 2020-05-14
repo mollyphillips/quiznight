8df6e193ec5fa02f26710d973e7b578a5aaa65b4
mollyphillips <molly.susan.phillips@outlook.com>
2020-05-14 21:57:37 +0800

    comments

--- server.py
+++ server.py
@@ -21,18 +21,28 @@ def begin_webapp():
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
+        # get the answer the user provided from the form
         answer = request.form["answer"]
+        # if it is correct
         if answer == session['currentQuestion']["answer"]:
+            # increase the score by 1
             session['score'] = session['score'] + 1
+        # increase the question number by 1
         session['questionNumber'] = session['questionNumber'] + 1
+        # if we are not finished
         if session['questionNumber'] < session['nQuestions']:
+            # grab the next question
             session['currentQuestion']  = session['questions'][session['questionNumber']]
+            # and render the question page
             return render_question()
         else:
-            return render_template("result.html",score = session['score'], time = int(time.time()-session['startTime']))
+            return render_result()
 
 def render_question():
     return render_template('quiz.html', question = session['currentQuestion']["question"], number = session['questionNumber'] + 1, score = session['score'], nQuestions = session['nQuestions'])
+
+def render_result():
+    return render_template("result.html",score = session['score'], time = int(time.time()-session['startTime']))
 
 if __name__ == '__main__':
    app.run(debug = True)
